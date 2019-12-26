368c398379fda69686fa07b0f3e9ff0eadb605e2
Leslie Zhai <zhaixiang@loongson.cn>
2019-12-26 20:53:55 +0800

    Initial build mpqeditor

--- mpqeditor/ui/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tamino/Dokumente/Projekte/Informatik/C++/vjasssdk/mpqeditor/ui
-# Build directory: /home/tamino/Dokumente/Projekte/Informatik/C++/vjasssdk/mpqeditor/ui
-# 
-# This file replicates the SUBDIRS() and ADD_TEST() commands from the source
-# tree CMakeLists.txt file, skipping any SUBDIRS() or ADD_TEST() commands
-# that are excluded by CMake control structures, i.e. IF() commands.
